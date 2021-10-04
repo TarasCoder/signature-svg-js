2f56418bb410f0d6a010203796843090171abd6e
TarasCoder <4kaskad@gmail.com>
2021-10-04 22:41:47 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+// var svg = document.getElementsByTagName('iframe')[0].contentWindow;
+// var pathdata = document.getElementById('pathdata');
+
+// function showSignature() {
+//   pathdata.textContent = svg.getSignature();
+// }
+
+// function clearSignature() {
+//   svg.clearSignature();
+//   pathdata.textContent = '';
+// }
